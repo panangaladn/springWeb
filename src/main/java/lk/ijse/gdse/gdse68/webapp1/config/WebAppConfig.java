c73298beca5eee44a56bbe8fd99a6bb820fac730
panangaladn <dunupa@gmail.com>
2024-08-18 08:52:19 +0530

    08_19_morning

--- src/main/java/lk/ijse/gdse/gdse68/webapp1/config/WebAppConfig.java
+++ src/main/java/lk/ijse/gdse/gdse68/webapp1/config/WebAppConfig.java
@@ -1,4 +1,9 @@
 package lk.ijse.gdse.gdse68.webapp1.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.gdse.gdse68.webapp1")
 public class WebAppConfig {
 }
--- src/main/java/lk/ijse/gdse/gdse68/webapp1/controller/DemoController.java
+++ src/main/java/lk/ijse/gdse/gdse68/webapp1/controller/DemoController.java
@@ -1,4 +1,38 @@
 package lk.ijse.gdse.gdse68.webapp1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
 public class DemoController {
+    @GetMapping(value = "/health")
+    // @GetMapping(value = "/healt?") මෙහි අවසානයට ? දැමූ විට http://localhost:8080/webApp1/demo/healtx එවන්න පුළුවන්
+    // @GetMapping(value = "/healt??") මෙහි අවසානයට ? දැමූ විට http://localhost:8080/webApp1/demo/healtxx? එවන්න පුළුවන්
+    // @GetMapping(value = "/health/*") මෙහි අවසානයට ? දැමූ විට http://localhost:8080/webApp1/demo/health/xxx එවන්න පුළුවන්
+    // @GetMapping(value = "/health/**") මෙහි අවසානයට ? දැමූ විට http://localhost:8080/webApp1/demo/health/xxx/yyy
+                                                                        // ඕනෑම තරමක් පාත් සේග්මන්ට් එවන්න පුළුවන්
+    public String healthCheck(){
+        return "DemoController OK";
+    }
+    @GetMapping//defult >> http://localhost:8080/webApp1/demo
+    public String otherText(){
+        return "Other text";
+    }
+
+    @GetMapping(value = "/pattern/{name}")
+    public String pathVariable(@PathVariable("name") String name) {
+        return "DemoController run perfectly with :"+name;
+    }
+    @GetMapping(value = "/patternRegex/{id:C\\d{3}}")
+    public String pathvariableWithRegex(@PathVariable("id") String id) {
+        return "DemoController run perfectly with :"+id;
+    }
+
+    @GetMapping(params = "test=all")
+    public String params(){
+        return "All are tested";
+    }
 }
